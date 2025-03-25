222fbede54adeeba3b98ed6bf150ea099149c6a6
Gabriel Hernandez <gabrielemanuel.hdz@gmail.com>
2025-03-24 19:35:39 -0600

    Create Suma.java
    Programa que suma dos numeros

---
+++ Suma.java
@@ -0,0 +1,10 @@
+//Programa que suma dos numeros
+public class suma{
+    public static void main(String args[])
+    {
+        int num1 = 18;
+        int num2 = 30;
+        int suma = num1 + num2;
+        System.out.print("La suma de " + num1 + " y de " + num2 + " es: " + suma);
+    }
+}
