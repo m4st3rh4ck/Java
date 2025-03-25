538091eb3d9ed4f82cfef10b2f405c2ae7c02e2f
Gabriel Hernandez <gabrielemanuel.hdz@gmail.com>
2025-03-24 19:50:27 -0600

    Create promedio.java
    Programa que calcula el promedio final de las calificaciones obtenidas de 3 examenes parciales

---
+++ promedio.java
@@ -0,0 +1,12 @@
+//Programa que calcula el promedio de un alumno
+public class promedio
+{
+    public static void main(String args[])
+    {
+        double  parcial1 = 6.7;
+        double parcial2 = 7;
+        double parcial3 = 8;
+        double promedio = (parcial1 + parcial2 + parcial3) / 3;
+        System.out.println("La calificacion final es: " + promedio);
+    }
+}
