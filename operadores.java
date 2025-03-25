87abc87bd04ec46a2e89e730fbd9bf6356ba077b
Gabriel Hernandez <gabrielemanuel.hdz@gmail.com>
2025-03-24 19:42:06 -0600

    Create operadores.java
    Operadores aritméticos en Java (suma, resta, división y multiplicación)

---
+++ operadores.java
@@ -0,0 +1,17 @@
+//Operadores Aritmeticos
+public class operadores
+{
+    public static void main(String args[])
+    {
+        float num1 = 18;
+        float num2 = 30;
+        float suma = num1 + num2;
+        float resta = num1 - num2;
+        float multiplicacion = num1 * num2;
+        float division = num1 / num2;
+        System.out.println("La suma de " + num1 + " y " + num2 + " es: " + suma);
+          System.out.println("\nLa resta de " + num1 + " y " + num2 + " es: " + resta);
+            System.out.println("\nLa multiplicacion de " + num1 + " y " + num2 + " es: " + multiplicacion);
+              System.out.println("\nLa division de " + num1 + " y " + num2 + " es: " + division);
+    }
+}
